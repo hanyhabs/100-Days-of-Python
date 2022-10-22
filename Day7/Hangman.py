c0d939529e7ea077c1ae98853fd1bdea1c9407f7
Hany Subhash <39477180+hanyhabs@users.noreply.github.com>
2022-10-22 23:40:05 +0530

    Update Hangman.py

--- Day7/Hangman.py
+++ Day7/Hangman.py
@@ -97,24 +97,6 @@ while not end_of_game:
     print(f"{' '.join(display)}")
 
     #Check if user has got all letters.
-    if "_" not in display:
-        end_of_game = True
-        print("You win.")
-    if lives == 6:
-      print(stages[0])
-    elif lives == 5:
-      print(stages[1])
-    elif lives == 4:
-      print(stages[2])
-    elif lives == 3:
-      print(stages[3])
-    elif lives == 2:
-      print(stages[4])
-    elif lives == 1:
-      print(stages[5])
-    elif lives == 0:
-      print(stages[6])
-    else:
-      print('Dead')
+    print(stages[lives])
 
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
